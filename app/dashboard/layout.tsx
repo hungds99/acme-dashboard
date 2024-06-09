4bed29a5c7d86cb42492ccf48f89640ba7252682
hungds99 <34293141+hungds99@users.noreply.github.com>
2024-06-09 19:53:11 +0700

    disable js demo

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,4 +1,8 @@
 import SideNav from '@/app/ui/dashboard/sidenav';
+import { Suspense } from 'react';
+import LatestInvoices from '../ui/dashboard/latest-invoices';
+import RevenueChart from '../ui/dashboard/revenue-chart';
+import { LatestInvoicesSkeleton } from '../ui/skeletons';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
@@ -6,7 +10,15 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       <div className="w-full flex-none md:w-64">
         <SideNav />
       </div>
-      <div className="grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+      {/* <div className="grow p-6 md:overflow-y-auto md:p-12">{children}</div> */}
+      <div className="grow p-6 md:overflow-y-auto md:p-12">
+        <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+          <RevenueChart />
+          <Suspense fallback={<LatestInvoicesSkeleton />}>
+            <LatestInvoices />
+          </Suspense>
+        </div>
+      </div>
     </div>
   );
 }
--- app/lib/data.ts
+++ app/lib/data.ts
@@ -19,8 +19,8 @@ export async function fetchRevenue() {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
 
-    console.log('Fetching revenue data...');
-    await new Promise((resolve) => setTimeout(resolve, 5000));
+    // console.log('Fetching revenue data...');
+    // await new Promise((resolve) => setTimeout(resolve, 1000));
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
@@ -36,6 +36,12 @@ export async function fetchRevenue() {
 export async function fetchLatestInvoices() {
   noStore();
   try {
+    // Artificially delay a response for demo purposes.
+    // Don't do this in production :)
+
+    console.log('Fetching latest invoices data...');
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
       FROM invoices
